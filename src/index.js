0985a4db87d409c84a1f391bdc4b594a2679e4ce
jain-ritik <44414761+jain-ritik@users.noreply.github.com>
2018-11-27 13:37:08 +0530

    list

--- src/index.js
+++ src/index.js
@@ -2,51 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Inc from "./inc";
 import Dec from "./dec";
+import List from "./list";
 import "./styles.css";
 
 class Parents extends React.Component {
-  /* onslick = () => {
-     this.inc.current.addOne;
-   };
-    <inc ref={this.inc} />
-    */
-  fun1 = () => {
-    this.inc.AddOne();
-    this.dec.AddOne();
-  };
-  fun2 = () => {
-    this.inc.SubOne();
-    this.dec.SubOne();
-  };
   render = () => {
     return (
       <div>
-        <button
-          onClick={() => {
-            this.fun1();
-          }}
-          class="button"
-        >
-          Increment
-        </button>
-        <Inc
-          ref={instance => {
-            this.inc = instance;
-          }}
-        />
-        <Dec
-          ref={instance => {
-            this.dec = instance;
-          }}
-        />
-        <button
-          onClick={() => {
-            this.fun2();
-          }}
-          class="button button3"
-        >
-          Decrement
-        </button>
+        <List />
       </div>
     );
   };
