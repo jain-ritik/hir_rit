2ac7f25fdd6e0f49a67070673a30df8d8eebf980
jain-ritik <44414761+jain-ritik@users.noreply.github.com>
2018-12-05 16:28:55 +0530

    modified_calci

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import update from "immutability-helper";
 import math from "mathjs";
 import "./styles.css";
 import "./App.css";
+var flag;
 class App extends Component {
   constructor() {
     super();
@@ -26,7 +27,8 @@ class App extends Component {
   };
 
   handleClick = e => {
-    const value = e.target.getAttribute("data-value");
+    const value = e.target.getAttribute("value");
+
     switch (value) {
       case "clear":
         this.setState({
@@ -35,14 +37,45 @@ class App extends Component {
         break;
       case "equal":
         this.calculateOperations();
+
         break;
-      default:
-        const newOperations = update(this.state.operations, {
-          $push: [value]
-        });
+      case "<-":
         this.setState({
-          operations: newOperations
+          operations: this.state.operations.splice(
+            0,
+            this.state.operations.length - 1
+          )
         });
+        break;
+
+      default:
+        if (
+          value == "/" ||
+          value == "^" ||
+          value == "*" ||
+          value == "-" ||
+          value == "+" ||
+          value == "."
+        ) {
+          flag++;
+          if (flag < 2) {
+            const newOperations = update(this.state.operations, {
+              $push: [value]
+            });
+            this.setState({
+              operations: newOperations
+            });
+          }
+        } else {
+          flag = 0;
+          const newOperations = update(this.state.operations, {
+            $push: [value]
+          });
+          this.setState({
+            operations: newOperations
+          });
+        }
+
         break;
     }
   };
@@ -50,33 +83,43 @@ class App extends Component {
     return (
       <div className="App">
         <Display data={this.state.operations} />
+
         <Buttons>
-          <Button
-            onClick={this.handleClick}
-            label="C"
-            value="clear"
-            class="Button"
-          />
-          <Button onClick={this.handleClick} label="7" value="7" />
-          <Button onClick={this.handleClick} label="4" value="4" />
-          <Button onClick={this.handleClick} label="1" value="1" />
-          <Button onClick={this.handleClick} label="0" value="0" />
+          <Button onClick={this.handleClick} label="C" value="clear" />
+          <Button onClick={this.handleClick} label="(" value="(" />
+          <Button onClick={this.handleClick} label="^" value="^" />
+          <Button onClick={this.handleClick} label=")" value=")" />
+
+          <br />
+          <Button onClick={this.handleClick} label="log" value="log" />
 
           <Button onClick={this.handleClick} label="/" value="/" />
-          <Button onClick={this.handleClick} label="8" value="8" />
-          <Button onClick={this.handleClick} label="5" value="5" />
-          <Button onClick={this.handleClick} label="2" value="2" />
           <Button onClick={this.handleClick} label="." value="." />
-
-          <Button onClick={this.handleClick} label="x" value="*" />
-          <Button onClick={this.handleClick} label="9" value="9" />
-          <Button onClick={this.handleClick} label="6" value="6" />
-          <Button onClick={this.handleClick} label="3" value="3" />
-          <Button label="" value="null" />
+          <Button onClick={this.handleClick} label="*" value="*" />
+          <br />
+          <Button onClick={this.handleClick} label="<-" value="<-" />
 
           <Button onClick={this.handleClick} label="-" value="-" />
+
           <Button onClick={this.handleClick} label="+" size="2" value="+" />
           <Button onClick={this.handleClick} label="=" size="2" value="equal" />
+          <br />
+          <Button onClick={this.handleClick} label="sin" value="sin" />
+          <Button onClick={this.handleClick} label="cos" value="cos" />
+          <Button onClick={this.handleClick} label="1" value="1" />
+          <Button onClick={this.handleClick} label="2" value="2" />
+          <br />
+          <Button onClick={this.handleClick} label="3" value="3" />
+          <Button onClick={this.handleClick} label="4" value="4" />
+          <Button onClick={this.handleClick} label="5" value="5" />
+          <Button onClick={this.handleClick} label="6" value="6" />
+          <br />
+          <Button onClick={this.handleClick} label="7" value="7" />
+          <Button onClick={this.handleClick} label="8" value="8" />
+          <Button onClick={this.handleClick} label="9" value="9" />
+          <Button onClick={this.handleClick} label="0" value="0" />
+
+          <br />
         </Buttons>
       </div>
     );
--- src/Button.js
+++ src/Button.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+
 class Button extends React.Component {
   render() {
     return (
       <div
         onClick={this.props.onClick}
-        className="Button button3"
-        data-size={this.props.size}
-        data-value={this.props.value}
+        className="button3"
+        size={this.props.size}
+        value={this.props.value}
       >
         {this.props.label}
       </div>
